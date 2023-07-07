6d2d4e31a9960c473b2809612a8a1d49a7cd5bbf
Elanchezhiyan <elanchezhiyan2440@gmail.com>
2023-07-08 00:38:03 +0530

    Sliders:
    1. Added a range slider and connected it to number of grids.
    2. Wrote relevant functions needed.
    3. Yet to modify labels and present an UI.
    
    I questioned my whole choice in career today, I won't lie.
    
    MURUGAAAAAAAAA!

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // create baseline 16x16 grid canvas
-n = 16;
 
+/*Slider to set the value of 'n' which determines the area of the grid */
+let slider = document.getElementsByClassName("slider")[0];
+slider.addEventListener("change", updateGridNumbers);
+window.addEventListener("load",updateGridNumbers)
+let n
+function updateGridNumbers(){
+    n = parseInt(slider.value);
+    console.log(n)
+    resizeGridNumbers(n);
+    document.getElementById("sliderLabel").textContent = slider.value;
+};
+
+
+
+//code to get canvas values and store in variables and get height valeu, filter it out and convert to number.
 const canvas = document.getElementById("canvas"); 
 const canvasStyles = window.getComputedStyle(canvas);
 const canvasHeight = canvasStyles.getPropertyValue("height");
@@ -8,24 +22,24 @@ let canvasHeightFiltered = +(canvasHeight.match(/\d+/)[0]);
 const canvasWidth = canvasStyles.getPropertyValue("width");
 let canvasWidthFiltered = +(canvasWidth.match(/\d+/)[0]);
 
-//filters out numbers alone from the value of canvasHeight and converts it to a number.
+
+/*gridHeight for setting heights of divs inside canvas div. Same for Width*/
 let gridHeight = (canvasHeightFiltered / n); 
 let gridWidth = (canvasWidthFiltered / n); 
-/*gridHeight for setting heights of divs inside canvas div. Same for Width*/
 
-console.log(n)
-console.log(typeof(n))
-console.log(canvasHeight)
-console.log(canvasHeightFiltered);
-console.log(typeof(canvasHeightFiltered))
-console.log(gridHeight); 
 
-for(i = 1; i <= n**2; i++){
-    const grids = document.createElement("div");
-    grids.className = "grids";
-    canvas.appendChild(grids);
-    canvas.style.gridTemplate = `repeat(${n}, 1fr)/repeat(${n}, 1fr)`
-    grids.style.height = `${gridHeight}px`;
-    grids.style.width = `${gridWidth}px`;
-    
-}
+// console.log(typeof(n))
+// console.log(canvasHeight)
+// console.log(canvasHeightFiltered);
+// console.log(typeof(canvasHeightFiltered))
+// console.log(gridHeight); 
+
+function resizeGridNumbers(){
+    for(i = 1; i <= n**2; i++){
+        const grids = document.createElement("div");
+        grids.className = "grids";
+        canvas.appendChild(grids);
+        canvas.style.gridTemplate = `repeat(${n}, 1fr)/repeat(${n}, 1fr)`
+        grids.style.height = `${gridHeight}px`;
+        grids.style.width = `${gridWidth}px`;
+}};
