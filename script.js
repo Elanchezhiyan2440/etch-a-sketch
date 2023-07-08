1bcb82b6afdda53d3264ed573171b5cdcaa6bb38
Elanchezhiyan <elanchezhiyan2440@gmail.com>
2023-07-08 18:08:00 +0530

    HOVER TO PAINT
    1. Wrote a JS function to add a class to divs inside the canvas when hovered over.
    2. Added a new CSS class "grids-hover" which is added on hover.
    3. Set resizeGridNumbers to call the new function "hoverToPaint" when its intitiated.
    4. (ALERT): Missing commit update for the previous commit before this:
        Added eventListener to page load so updateGridNumbers get called on page load,
        which in turn calls resizeGridNumbers, which now calls hoverToPaint.
    
    enna kaapaathu ya en appa Murugaa...

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-// create baseline 16x16 grid canvas
-
-/*Slider to set the value of 'n' which determines the area of the grid */
+//code to get canvas values and store in variables and get height valeu, filter it out and convert to number.
 let slider = document.getElementsByClassName("slider")[0];
 slider.addEventListener("change", updateGridNumbers);
 window.addEventListener("load",updateGridNumbers)
-let n
-function updateGridNumbers(){
-    n = parseInt(slider.value);
-    console.log(n)
-    resizeGridNumbers(n);
-    document.getElementById("sliderLabel").textContent = slider.value;
-};
-
-
-
-//code to get canvas values and store in variables and get height valeu, filter it out and convert to number.
 const canvas = document.getElementById("canvas"); 
 const canvasStyles = window.getComputedStyle(canvas);
 const canvasHeight = canvasStyles.getPropertyValue("height");
@@ -22,18 +9,21 @@ let canvasHeightFiltered = +(canvasHeight.match(/\d+/)[0]);
 const canvasWidth = canvasStyles.getPropertyValue("width");
 let canvasWidthFiltered = +(canvasWidth.match(/\d+/)[0]);
 
-
+//VARIABLES:
+let n
 /*gridHeight for setting heights of divs inside canvas div. Same for Width*/
 let gridHeight = (canvasHeightFiltered / n); 
 let gridWidth = (canvasWidthFiltered / n); 
 
+// FUNCTIONS:
+/*Slider to set the value of 'n' which determines the area of the grid */
+function updateGridNumbers(){
+    n = parseInt(slider.value);
+    console.log(n)
+    resizeGridNumbers(n);
+};
 
-// console.log(typeof(n))
-// console.log(canvasHeight)
-// console.log(canvasHeightFiltered);
-// console.log(typeof(canvasHeightFiltered))
-// console.log(gridHeight); 
-
+//function that places grids inside canvas and calls hover to paint.
 function resizeGridNumbers(){
     for(i = 1; i <= n**2; i++){
         const grids = document.createElement("div");
@@ -42,4 +32,11 @@ function resizeGridNumbers(){
         canvas.style.gridTemplate = `repeat(${n}, 1fr)/repeat(${n}, 1fr)`
         grids.style.height = `${gridHeight}px`;
         grids.style.width = `${gridWidth}px`;
-}};
+    };
+    hoverToPaint();
+   };
+
+//Hover to Paint function:
+function hoverToPaint(){document.querySelectorAll(".grids").forEach(elem => {
+    elem.addEventListener('mouseover', e => e.target.classList.add('grids-hover'));
+  })};
